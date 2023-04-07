b7c102a74fa0bf750a591dbb83f34481eb922b68
Maxim Ketels <maxim.ketels@outlook.com>
2023-04-07 16:34:18 +0200

    added l-system string expansion

--- lSystems.py
+++ lSystems.py
@@ -1,5 +1,24 @@
 def main():
-    print("main")
+    axiom = "A"
+    rules = {
+        "A": "AA+[+A-A-A]-[-A+A+A]"
+    }
+    print(l_system_string_expansion(axiom, rules, 2))
+
+
+def l_system_string_expansion(axiom: str, rules: dict, iterations: int) -> str:
+    current_string = axiom
+    keys = rules.keys()
+    for iter in range(iterations):
+        new_string = ""
+        for symbol in current_string:
+            if symbol in keys:
+                new_string += rules[symbol]
+
+        current_string = new_string
+
+    return current_string
+
 
 if __name__ == "__main__":
     main()
