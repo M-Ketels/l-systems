59dd565a823bf294855fd9fb7aae1bed0409b95c
Maarten Dorhetso <maarten_do@hotmail.com>
2023-04-08 13:22:00 +0200

    heading draw adjustment

--- lSystems_turtle.py
+++ lSystems_turtle.py
@@ -3,9 +3,9 @@ import lSystems as lSys
 
 
 def main():
-    draw_turtle(5, "Examples/KochCurve.json", 1000)
-    #draw_turtle(5, "Examples/SierpinskiTriangle.json", 1000)
-    #draw_turtle(6, "Examples/fractalplant.json", 1000)
+    # draw_turtle(5, "Examples/KochCurve.json", 1000)
+    # draw_turtle(5, "Examples/SierpinskiTriangle.json", 1000)
+    draw_turtle(6, "Examples/fractalplant.json", 1000)
 
 
 def trans_to_dict(json_file_loc: str) -> dict:
@@ -16,14 +16,13 @@ def trans_to_dict(json_file_loc: str) -> dict:
 
 def draw_turtle(iteration: int, json_file_loc: str, draw_speed: int) -> None:
     to_draw_string = lSys.json_str_expansion(json_file_loc, iteration)
-    print(to_draw_string)
     translations = trans_to_dict(json_file_loc)
-    print(translations)
 
     position_stack = []
     heading_stack = []
 
     draw = turtle.Turtle()
+    draw.setheading(90)
     turtle.Screen()
     draw.speed(draw_speed)
     for char in to_draw_string:
@@ -38,18 +37,18 @@ def draw_turtle(iteration: int, json_file_loc: str, draw_speed: int) -> None:
             else:
                 draw.left(abs(value))
         elif "stop" == to_do:
-            draw.heading()
+            draw.setheading(90)
         elif "save" == to_do:
             position_stack.append(draw.pos())
             heading_stack.append(draw.heading())
-            #saved_pos = draw.pos()
+            # saved_pos = draw.pos()
         elif "rest" == to_do:
             draw.penup()
             popped_position = position_stack.pop()
             draw.goto(popped_position[0], popped_position[1])
             popped_heading = heading_stack.pop()
             draw.setheading(popped_heading)
-            #draw.goto(saved_pos[0], saved_pos[1])
+            # draw.goto(saved_pos[0], saved_pos[1])
             draw.pendown()
     turtle.done()
 
