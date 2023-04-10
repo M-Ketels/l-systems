9c708d5489fe2f745c07a1ee0431fcd0f39ac68c
Maxim Ketels <maxim.ketels@outlook.com>
2023-04-10 12:06:53 +0200

    added json parsing tests

--- test_lsystems.py
+++ test_lsystems.py
@@ -8,7 +8,7 @@ To run all test functions: type "pytest" in the terminal
 """
 
 ####################################################
-# json reading tests
+# json parsing tests
 ####################################################
 
 def test_json_asignment_plant_axiom():
@@ -21,6 +21,30 @@ def test_json_asignment_plant_rules():
 
 def test_json_asignment_plant_translations():
     json_dict = ls.json_to_dict("Examples/AsignmentPlant.json")
+    assert json_dict["translations"]["A"] == ["draw", 5]
+    assert json_dict["translations"]["+"] == ["angle", 22.5]
+    assert json_dict["translations"]["-"] == ["angle", -22.5]
+    assert json_dict["translations"]["["] == ["push"]
+    assert json_dict["translations"]["]"] == ["pop"]
+
+
+def test_json_fractal_plant_axiom():
+    json_dict = ls.json_to_dict("Examples/fractalplant.json")
+    assert json_dict["axiom"] == "X"
+
+def test_json_fractal_plant_rules():
+    json_dict = ls.json_to_dict("Examples/fractalplant.json")
+    assert json_dict["rules"]["X"] == "F+[[X]-X]-F[-FX]+X"
+    assert json_dict["rules"]["F"] == "FF"
+
+def test_json_fractal_plant_translations():
+    json_dict = ls.json_to_dict("Examples/fractalplant.json")
+    assert json_dict["translations"]["F"] == ["draw", 5]
+    assert json_dict["translations"]["X"] == ["nop"]
+    assert json_dict["translations"]["+"] == ["angle", 25]
+    assert json_dict["translations"]["-"] == ["angle", -25]
+    assert json_dict["translations"]["["] == ["push"]
+    assert json_dict["translations"]["]"] == ["pop"]
 
 
 
